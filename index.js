d7d90f65f8a78933091a27971b3ce954e482345c
Mitchell Turnbaugh <MitchellTurnbaughCProg@gmail.com>
2025-11-21 08:46:48 -0500

    Made the divs clickable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function remove(collection,item){
     const index = collection.indexOf(item);
-    console.log(index)
     collection.splice(index,1);
     return collection;
 }
@@ -12,7 +11,6 @@ function choice(collection){
 }
 function randomCards(){
     const cards = document.getElementsByClassName("card");
-    console.log(cards)
     let images = ["acorn_icon.png","campfire_icon.png","caramel_apple_icon.jpg",
     "fall_leaf_icon.png","fall_tree_icon.png","hedgehog_icon.png","pinecone_icon.jpg",
     "pumpkin_pie_icon.png","squirrel.png","umbrella_icon.png","acorn_icon.png",
@@ -24,10 +22,8 @@ function randomCards(){
         img = choice(images);
         i.className = `card ${img}`;
         images = remove(images,img);
-        console.log(images);
     }   
 }
 function flipCard(card){
-    console.log(card.className)
     card.src = `resources/index/${card.className.split(" ")[1]}`;
 }
